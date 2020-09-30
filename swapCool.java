6f3a3311b796d86eadd4861e622da8db2b96b471
Samrat <souvik.samrat.003@gmail.com>
2020-10-01 00:24:32 +0530

    new file added

---
+++ swapCool.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class swapCool {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.format("a=%d and b=%d",b,a);
+
+	}
+
+}
+
